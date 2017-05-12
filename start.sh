5183baf63eae469155148f77d3bc5694afe0f9c4
Andrey Chernov <achernov@byterox.com>
2017-05-12 16:24:59 +0700

    skip dev-uninstall

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo ansible-playbook -i inventory provision.yml --skip-tags "uninstall-toolchain"
-sudo ansible-playbook -i inventory provision.yml --tags "uninstall-toolchain"
+sudo ansible-playbook -i inventory provision.yml --skip-tags "uninstall-toolchain,dev-uninstall"
+sudo ansible-playbook -i inventory provision.yml --tags "uninstall-toolchain,dev-uninstall"
